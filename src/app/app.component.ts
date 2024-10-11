18907a98d36d0bb5390200c91da2d41f4bce4d98
111979_DiFrancisco_Mateo <111979@tecnicatura.frc.utn.edu.ar>
2024-10-11 16:27:59 -0300

    Primer Commit Proyecto Base
    primer commit del proyecto base con la librería core instalada, con bootstrap instalado y con el DataTables funcionando, carpetas de components, models, etc ya creadas

---
+++ src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "ngx-dabd-2w1-core";
+import { MenuItems } from 'ngx-dabd-2w1-core';
+
+//imports para el Datatables (el segundo import termina en bs5 para que tenga 
+//los estilos de bootstrap)
+import $ from 'jquery';
+import 'datatables.net'
+import 'datatables.net-bs5';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit{
+  
+  ngOnInit(): void {
+      //para iniciar el datatables, configuraciones basicas
+      //se hace en el onInit para que se carge al cargar la pag
+    $('#myTable').DataTable({
+      paging: true,
+      searching: true,
+      ordering: true,
+      lengthChange: true,
+      pageLength: 10
+    });
+  }
+  title = 'template-app';
+
+  //navbar
+  visibleSection: string = '';
+  items: MenuItems[] = [
+    {
+      key: 'menu1',
+      name: 'Lista Gastos',
+      active: true,
+      //icon: 'alarm' 
+    },
+    {
+      key: 'menu2',
+      name: 'Registrar Gastos',
+      active: true
+    },
+    {
+      key: 'menu3',
+      name: 'disabled',
+      active: false,
+      disabled: true
+    }
+  ];
+
+  //navbar
+  onMenuVisited(key: string) {
+    this.visibleSection = key;
+  }
+}
+
