a14b5963aa876514c74e44a380a5742a76e4edfb
Santino Consigli <406030@tecnicatura.frc.utn.edu.ar>
2024-10-13 13:19:41 -0300

    visualizer expenses like admin, work with filter dates, datatables and implementation note of credit or delete logic expense

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Bill } from '../models/bill';
@@ -7,6 +7,8 @@ import { Bill } from '../models/bill';
   providedIn: 'root'
 })
 export class BillService {
+ 
+  
   private apiUrl = 'http://localhost:8080/expenses/getByFilters';
 
   constructor(private http: HttpClient) {}
@@ -18,6 +20,21 @@ export class BillService {
     const dateFrom = firstDayOfMonth.toISOString().split('T')[0];
     const dateTo = lastDayOfMonth.toISOString().split('T')[0];
     const urlWithFilters = `${this.apiUrl}?dateFrom=${dateFrom}&dateTo=${dateTo}`;
+    return this.http.get<Bill[]>(urlWithFilters);
+  }
+  deleteLogicBill(id: number): Observable<void> {
+    const url = 'http://localhost:8080/expenses?id='+id;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.delete<void>(url, { headers });
+  }
+  createNoteOfCredit(failedBillId: number | null) {
+    const url = 'http://localhost:8080/expenses/note_credit?id='+failedBillId;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.delete<void>(url, { headers });
+  }
+  getBillsByDateRange(formattedDateFrom: string, formattedDateTo: string) :Observable<Bill[]> {
+    const urlWithFilters = `${this.apiUrl}?dateFrom=${formattedDateFrom}&dateTo=${formattedDateTo}`;
+    console.log(urlWithFilters)
     return this.http.get<Bill[]>(urlWithFilters);
   }
   
