e48ebb30c8d2fc69cb1684a4c3b0904ab0f8b2f4
Santino Consigli <406030@tecnicatura.frc.utn.edu.ar>
2024-10-18 14:21:51 -0300

    add debounce time petitions

--- src/app/components/expenses-view-gastos-admin/view-gastos-admin.component.ts
+++ src/app/components/expenses-view-gastos-admin/view-gastos-admin.component.ts
@@ -5,12 +5,12 @@ import { CommonModule } from '@angular/common';
 //Imports para el DataTable
 import moment from 'moment';
 import $ from 'jquery';
-import 'datatables.net'
+import 'datatables.net';
 import 'datatables.net-bs5';
 import { DistributionList } from '../../models/distributionList';
 import { Instalmentlist } from '../../models/installmentList';
 import { BillService } from '../../services/billServices/bill.service';
-import { debounceTime, distinctUntilChanged, filter, mergeMap, of, Subject, switchMap, takeUntil, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, finalize, mergeMap, of, Subject, switchMap, takeUntil, tap } from 'rxjs';
 
 
 @Component({
@@ -29,11 +29,12 @@ export class ViewGastosAdminComponent implements OnInit {
   distributionList: DistributionList[] = [];
   installmentList: Instalmentlist[] = [];
   failedBillId: number = 0;
-  showErrorModal = false;
-  isInitial: Boolean = true;
+  isLoading = false;
   private dateChangeSubject = new Subject<{ from: string, to: string }>();
   private unsubscribe$ = new Subject<void>();
-  @ViewChild('errorModal') errorModal: ElementRef | undefined;
+  @ViewChild('modalNoteCredit') modalNoteCredit!: ElementRef;
+  @ViewChild('modalConfirmDelete') modalConfirmDelete!: ElementRef;
+
 
   bills: Bill[] = [];
   filterBills: Bill[] = [];
@@ -46,46 +47,70 @@ export class ViewGastosAdminComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.loadDates();
-    this.setupDateChangeObservable();
-    this.configDataTable();
-    this.filterDataOnInit();
-  }
-  filterDataOnInit() {
-    this.billService.getBillsByDateRange(this.dateFrom, this.dateTo).subscribe({
-      next: (response: Bill[]) => {
-        this.bills = response;
-        this.loadBillsFiltered();
-      },
-      error: (error) => {
-        console.error('Error fetching initial bills:', error);
-      }
-    });
-  }
+  this.loadDates();
+  this.setupDateChangeObservable();
+  this.configDataTable();
+  this.filterDataOnInit();
+}
 
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
+filterDataOnInit() {
+  this.isLoading = true;  // Activar spinner
+  this.billService.getBillsByDateRange(this.dateFrom, this.dateTo).pipe(
+    finalize(() => {
+      this.isLoading = false;
+      this.cdRef.detectChanges();
+    })
+  ).subscribe({
+    next: (response: Bill[]) => {
+      this.bills = response;
+      this.loadBillsFiltered();
+    },
+    error: (error) => {
+      console.error('Error fetching initial bills:', error);
+    }
+  });
+}
 
-  private setupDateChangeObservable() {
-    this.dateChangeSubject.pipe(
-      debounceTime(3000),
-      distinctUntilChanged((prev, curr) => 
-        prev.from === curr.from && prev.to === curr.to
-      ),
-      switchMap(({ from, to }) => this.billService.getBillsByDateRange(from, to)),
-      takeUntil(this.unsubscribe$)
-    ).subscribe({
-      next: (response: Bill[]) => {
-        this.bills = response;
-        this.loadBillsFiltered();
-      },
-      error: (error) => {
-        console.error('Error fetching bills:', error);
-      }
-    });
-  }
+ngOnDestroy(): void {
+  this.unsubscribe$.next();
+  this.unsubscribe$.complete();
+}
+
+private setupDateChangeObservable() {
+  // Set loading true immediately when method is called
+  this.isLoading = true;
+  this.cdRef.detectChanges();
+
+  this.dateChangeSubject.pipe(
+    tap(() => {
+      // Set loading true on each new emission
+      this.isLoading = true;
+      this.cdRef.detectChanges();
+    }),
+    debounceTime(3000),
+    distinctUntilChanged((prev, curr) => 
+      prev.from === curr.from && prev.to === curr.to
+    ),
+    switchMap(({ from, to }) => {
+      this.isLoading=false
+      return this.billService.getBillsByDateRange(from, to);
+    }),
+    finalize(() => {
+      // Set loading false when the observable completes or errors
+      this.isLoading = false;
+      this.cdRef.detectChanges();
+    }),
+    takeUntil(this.unsubscribe$)
+  ).subscribe({
+    next: (response: Bill[]) => {
+      this.bills = response;
+      this.loadBillsFiltered();
+    },
+    error: (error) => {
+      console.error('Error fetching bills:', error);
+    }
+  });
+}
 
   filterDataOnChange() {
     this.dateChangeSubject.next({ from: this.dateFrom, to: this.dateTo });
@@ -97,7 +122,10 @@ export class ViewGastosAdminComponent implements OnInit {
     this.maxDateTo = this.dateTo;
     this.dateFrom = today.subtract(1, 'month').format('YYYY-MM-DD');
   }
-
+  confirmDeleteExpense(){
+     this.deleteBill(this.failedBillId)
+     this.closeModal(this.modalConfirmDelete)
+  }
   deleteBill(id: number) {
     this.billService.deleteLogicBill(id).subscribe({
       next: () => {
@@ -108,8 +136,7 @@ export class ViewGastosAdminComponent implements OnInit {
       error: (error) => {
         console.error(`Error al eliminar la factura con ID ${id}:`, error);
         if (error.error.status == 409 && error.error.message == "Expense has related bill installments") {
-          this.showModalToNoteCredit();
-          this.failedBillId = id;
+          this.openModal(this.modalNoteCredit)
         } else {
           alert('Ocurrió un error al eliminar el gasto');
         }
@@ -117,30 +144,24 @@ export class ViewGastosAdminComponent implements OnInit {
     });
   }
 
-  showModalToNoteCredit() {
-    this.showErrorModal = true;
+ 
+  openModal(modal: ElementRef | HTMLDivElement) {
+    const element = modal instanceof ElementRef ? modal.nativeElement : modal;
+    element.style.display = 'block';
+    element.classList.add('show');
+    document.body.classList.add('modal-open');
     this.cdRef.detectChanges();
-    setTimeout(() => {
-      const modalElement = document.getElementById('errorModal');
-      if (modalElement) {
-        modalElement.style.display = 'block';
-        modalElement.classList.add('show');
-      }
-    }, 0);
   }
 
   loadBillsFiltered() {
     const dataTable = $('#myTable').DataTable();
     dataTable.clear().rows.add(this.bills).draw();
   }
-
-  closeModal() {
-    this.showErrorModal = false;
-    if (this.errorModal) {
-      this.errorModal.nativeElement.style.display = 'none';
-      this.errorModal.nativeElement.classList.remove('show');
-      document.body.classList.remove('modal-open');
-    }
+  closeModal(modal: ElementRef | HTMLDivElement) {
+    const element = modal instanceof ElementRef ? modal.nativeElement : modal;
+    element.style.display = 'none';
+    element.classList.remove('show');
+    document.body.classList.remove('modal-open');
     this.cdRef.detectChanges();
     this.loadBillsFiltered();
   }
@@ -151,12 +172,11 @@ export class ViewGastosAdminComponent implements OnInit {
         console.log(`Gasto con ID ${this.failedBillId} se le creó nota de crédito con éxito`);
         this.filterDataOnChange();
         alert('Se realizó la nota de crédito con éxito');
-        this.closeModal();
+        this.closeModal(this.modalNoteCredit);
       },
       error: (error) => {
         console.error(`Error al realizar la nota de crédito del gasto con ID ${this.failedBillId}:`, error);
         alert('No se pudo realizar la nota de crédito con éxito');
-        this.showModalToNoteCredit();
       }
     });
   }
@@ -199,8 +219,35 @@ export class ViewGastosAdminComponent implements OnInit {
         {
           title: "Opciones",
           data: null,
-          defaultContent: '<button class="btn btn-danger">Eliminar</button>'
-        }
+          orderable: false,
+          className: 'text-center',
+          render: function(data, type, row) {
+            return `
+               <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Opciones
+      </button>
+      <ul class="dropdown-menu">
+        <li>
+          <a class="dropdown-item text-info btn-view" href="#">
+            <i class="fas fa-eye"></i> Ver más
+          </a>
+        </li>
+        <li>
+          <a class="dropdown-item text-secondary btn-edit" href="#">
+            <i class="fas fa-edit"></i> Editar
+          </a>
+        </li>
+        <li>
+          <a class="dropdown-item text-danger btn-delete" href="#">
+            <i class="fas fa-trash"></i> Eliminar
+          </a>
+        </li>
+      </ul>
+    </div>
+            `;
+          }
+      },
       ],
       language: {
         processing: "Procesando...",
@@ -208,7 +255,7 @@ export class ViewGastosAdminComponent implements OnInit {
         lengthMenu: "Mostrar MENU registros",
         info: "Mostrando del _START_ al _END_ de _TOTAL_ registros",
         infoEmpty: "Mostrando 0 registros",
-        infoFiltered: "(filtrado de MAX registros totales)",
+        infoFiltered: "(filtrado de _MAX_ registros totales)",
         loadingRecords: "Cargando...",
         zeroRecords: "No se encontraron resultados",
         emptyTable: "No hay datos disponibles en la tabla",
@@ -220,14 +267,38 @@ export class ViewGastosAdminComponent implements OnInit {
       }
     }
     });
+    const table = $('#myTable').DataTable();
 
-    $('#myTable tbody').on('click', '.btn-danger', (event) => {
+    $('#myTable tbody').on('click', '.btn-view', (event) => {
       const row = $(event.currentTarget).closest('tr');
-      const rowData = $('#myTable').DataTable().row(row).data();
-      this.deleteBill(rowData.id);
+      const rowData = table.row(row).data();
+      this.viewBillDetails(rowData.id);
     });
+
+    $('#myTable tbody').on('click', '.btn-edit', (event) => {
+      const row = $(event.currentTarget).closest('tr');
+      const rowData = table.row(row).data();
+      this.editBill(rowData.id);
+    });
+
+    $('#myTable tbody').on('click', '.btn-delete', (event) => {
+      const row = $(event.currentTarget).closest('tr');
+      const rowData = table.row(row).data();
+      this.failedBillId= rowData.id
+      this.openModal(this.modalConfirmDelete)
+    });
+    
+    
+  }
+  editBill(id: any) {
+    throw new Error('Method not implemented.');
+  }
+  viewBillDetails(id: any) {
+    throw new Error('Method not implemented.');
   }
 }
  
+
+
 
 
