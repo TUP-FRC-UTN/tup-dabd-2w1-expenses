6c67aae36270fb927ccb31b93c17c18ec0ed2376
114032_BAEZ_NICOLAS <114032@tecnicatura.frc.utn.edu.ar>
2024-10-23 23:54:13 -0300

    Fix TimeLapse

--- src/app/components/expenses-view-owner/view-owner-expense/view-owner-expense.component.ts
+++ src/app/components/expenses-view-owner/view-owner-expense/view-owner-expense.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import 'datatables.net';
 import 'datatables.net-bs5';
 import moment from 'moment';
@@ -12,6 +12,8 @@ import { ProviderViewOwnerService } from '../../../services/viewOwnerServices/pr
 import * as XLSX from 'xlsx';   // Para exportar a Excel
 import jsPDF from 'jspdf';      // Para exportar a PDF
 import 'jspdf-autotable';       // Para generar tablas en PDF
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, tap, finalize, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view-owner-expense',
@@ -20,8 +22,7 @@ import 'jspdf-autotable';       // Para generar tablas en PDF
   templateUrl: './view-owner-expense.component.html',
   styleUrls: ['./view-owner-expense.component.scss'],
 })
-
-export class ViewOwnerExpenseComponent implements OnInit {
+export class ViewOwnerExpenseComponent implements OnInit, OnDestroy {
   bills: BillViewOwner[] = [];
   filteredBills: BillViewOwner[] = [];
   providersMap: { [key: string]: string } = {};
@@ -30,213 +31,187 @@ export class ViewOwnerExpenseComponent implements OnInit {
   fechaHasta: string = '';
   maxDateTo: string = '';
 
+  private dateChangeSubject = new Subject<{ from: string, to: string }>();
+  private unsubscribe$ = new Subject<void>();
+  isLoading = false;
+
   constructor(
-    private billService: BillViewOwnerService, 
+    private billService: BillViewOwnerService,
     private providerService: ProviderViewOwnerService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    $.fn.dataTable.ext.type.order['date-moment-pre'] = function (d: string) {
-      return moment(d, 'DD/MM/YYYY').unix();  // Convertir la fecha a timestamp para que pueda ordenarse
-    };
-    //this.loadData();
     this.loadDates();
-    this.filtrarPorFecha();
-    //this.initDataTable(this.bills);
+    this.setupDateChangeObservable();
+  
+    this.filterDataOnChange();
   }
 
-  // loadData(): void {
-  //   this.providerService.getProviders().subscribe(providers => {
-  //     this.providersMap = providers.reduce((acc: { [key: string]: string }, provider) => {
-  //       acc[provider.id] = provider.nombre;
-  //       return acc;
-  //     }, {});
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 
-  //     this.billService.getBillsOnInit().subscribe((data: BillViewOwner[]) => {
-  //       console.log('Datos de facturas recibidos:', data);
-  //       this.bills = data.map(bill => ({
-  //         ...bill,
-  //         providerId: this.providersMap[bill.providerId] || 'Desconocido'
-  //       }));
-  //       this.filteredBills = [...this.bills];
-  //       setTimeout(() => {
-  //         this.initDataTable(this.filteredBills);
-  //       }, 0);
-  //     });
-  //   });
-  // }
+  // Configuración del observable para escuchar cambios en las fechas
+  private setupDateChangeObservable() {
+    this.dateChangeSubject.pipe(
+      tap(() => {
+        this.isLoading = true;
+      }),
+      debounceTime(1200),
+      distinctUntilChanged((prev, curr) => 
+        prev.from === curr.from && prev.to === curr.to
+      ),
+      switchMap(({ from, to }) => {
+        return this.billService.getBillsByOwnerIdAndDateFromDateTo(223, from, to);
+      }),
+      takeUntil(this.unsubscribe$)
+    ).subscribe({
+      next: (response: BillViewOwner[]) => {
+        this.bills = response;
+        this.configDataTable();
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching bills:', error);
+        this.isLoading = false;
+      }
+    });
+  }
 
+  // Cargar fechas por defecto (último mes hasta hoy)
   loadDates() {
     const today = new Date();
     const yyyy = today.getFullYear();
     const mm = String(today.getMonth() + 1).padStart(2, '0');
     const dd = String(today.getDate()).padStart(2, '0');
     this.fechaHasta = `${yyyy}-${mm}-${dd}`;
-    this.maxDateTo=`${yyyy}-${mm}-${dd}`;
+    this.maxDateTo = `${yyyy}-${mm}-${dd}`;
+  
     const past = new Date();
-    past.setMonth(past.getMonth() - 1); 
+    past.setMonth(past.getMonth() - 1);
     const pastyyyy = past.getFullYear();
     const pastmm = String(past.getMonth() + 1).padStart(2, '0');
     const pastdd = String(past.getDate()).padStart(2, '0');
     this.fechaDesde = `${pastyyyy}-${pastmm}-${pastdd}`;
+  
+    // Realizar la búsqueda automáticamente después de cargar las fechas por defecto
+    this.filterDataOnChange();
   }
 
-  // getBillsByOwnerId(ownerId: number): void {
-  //   this.billService.getBillsByOwnerIdAndDateFromDateTo(ownerId,this.fechaDesde,this.fechaHasta).subscribe((data: BillViewOwner[]) => {
-  //     console.log('Datos de facturas recibidos:', data);
-  //     this.bills = data.map(bill => ({
-  //       ...bill,
-  //       providerId: this.providersMap[bill.providerId] || 'Desconocido'
-  //     }));
-  //     this.filteredBills = [...this.bills];
-  //     setTimeout(() => {
-  //       this.initDataTable(this.filteredBills);
-  //     }, 0);
-  //   });
-  // }
-
-
-  filtrarPorFecha(): void {
-    const formattedDateFrom = this.fechaDesde;
-    const formattedDateTo = this.fechaHasta;
-
-    this.billService.getBillsByOwnerIdAndDateFromDateTo(223,formattedDateFrom, formattedDateTo).subscribe(
-      (filteredBills) => {
-        this.bills = filteredBills; 
-        this.configDataTable();
-        console.log(filteredBills);
-      },
-      (error) => {
-        console.error('Error al filtrar los gastos:', error);
-      }
-    );
+  // Cuando cambia alguna de las fechas
+  filterDataOnChange() {
+    this.dateChangeSubject.next({ from: this.fechaDesde, to: this.fechaHasta });
   }
 
-  // Función para exportar a Excel
-  exportToExcel(): void {
-    const table = $('#myTable').DataTable();
-    const filteredData = table.rows({ search: 'applied' }).data().toArray();
-
-    const worksheet = XLSX.utils.json_to_sheet(filteredData);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Gastos');
-    XLSX.writeFile(workbook, 'gastos.xlsx');
-  }
-
-  // Función para exportar a PDF
+  // Exportar a PDF
   exportToPDF(): void {
     const doc = new jsPDF();
+  
+    const pageTitle = 'Listado de Gastos';
+    doc.setFontSize(18);
+    doc.text(pageTitle, 15, 10);
+    doc.setFontSize(12);
+  
+    doc.text(`Fechas: Desde ${moment(this.fechaDesde).format('DD/MM/YYYY')} hasta ${moment(this.fechaHasta).format('DD/MM/YYYY')}`, 15, 20);
+  
     const table = $('#myTable').DataTable();
     const filteredData = table.rows({ search: 'applied' }).data().toArray();
-
-    const pdfData = filteredData.map(bill => [
-      bill.description,
-      bill.categoryDescription,
-      bill.providerId,
-      bill.expenseType,
-      moment(bill.expenseDate).format('DD/MM/YYYY'),
-      `$${bill.amount}`
-    ]);
-
+  
+    const pdfData = filteredData.map(bill => {
+      const [category, ...rest] = bill.description.split(' - ');
+      return [
+        rest.join(' - '),
+        category,
+        bill.providerId,
+        bill.expenseType,
+        moment(bill.expenseDate).format('DD/MM/YYYY'),
+        `$${bill.amount}`
+      ];
+    });
+  
+    let pageCount = 0;
+  
     (doc as any).autoTable({
       head: [['Descripción', 'Categoría', 'Proveedor', 'Tipo de Gasto', 'Fecha', 'Monto']],
       body: pdfData,
+      startY: 30, 
+      theme: 'grid',
+      margin: { top: 30, bottom: 20 },
+      didDrawPage: function (data: any) {
+        pageCount++;
+        const pageSize = doc.internal.pageSize;
+        const pageHeight = pageSize.height || pageSize.getHeight();
+  
+        doc.setFontSize(10);
+        const text = `Página ${pageCount}`;
+        const textWidth = doc.getTextWidth(text);
+        doc.text(text, (pageSize.width / 2) - (textWidth / 2), pageHeight - 10);
+      }
     });
-
-    doc.save('gastos.pdf');
+  
+    doc.save('listado_gastos.pdf');
   }
-  // loadBillsFiltered() {
-  //   const dataTable = $('#myTable').DataTable();
-  //   dataTable.clear();
-  //   dataTable.rows.add(this.bills);
-  //   dataTable.draw();
-  // }
-  // formatDate(date: string) {
-  //   const parsedDate = new Date(date);
-  //   const year = parsedDate.getFullYear();
-  //   const month = (parsedDate.getMonth() + 1).toString().padStart(2, '0')
-  //   const day = parsedDate.getDate().toString().padStart(2, '0'); 
-  //   return `${year}-${month}-${day}`;
-  // }
+  
+  //Exportar a Excel
+  exportToExcel(): void {
+    const table = $('#myTable').DataTable();
+    const filteredData = table.rows({ search: 'applied' }).data().toArray();
+  
+    const encabezado = [
+      [`Listado de Gastos`],
+      [`Fechas: Desde ${moment(this.fechaDesde).format('DD/MM/YYYY')} hasta ${moment(this.fechaHasta).format('DD/MM/YYYY')}`],
+      [],
+      ['Descripción', 'Categoría', 'Proveedor', 'Tipo de Gasto', 'Fecha', 'Monto']
+    ];
+  
+    const excelData = filteredData.map(bill => {
+      const [category, ...rest] = bill.description.split(' - ');
+      return [
+        rest.join(' - '),
+        category,
+        bill.providerId,
+        bill.expenseType === 'NOTE_OF_CREDIT' ? 'NOTA DE CRÉDITO' : bill.expenseType,
+        moment(bill.expenseDate).format('DD/MM/YYYY'),
+        `$${bill.amount}`
+      ];
+    });
+  
+    const worksheetData = [...encabezado, ...excelData];
+    const worksheet = XLSX.utils.aoa_to_sheet(worksheetData);
+    
+    worksheet['!cols'] = [
+      { wch: 30 },  // Ancho de la columna de descripción
+      { wch: 20 },  // Ancho de la columna de categoría
+      { wch: 20 },  // Ancho de la columna de proveedor
+      { wch: 20 },  // Ancho de la columna de tipo de gasto
+      { wch: 15 },  // Ancho de la columna de fecha
+      { wch: 10 }   // Ancho de la columna de monto
+    ];
+  
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Gastos');
+  
+    XLSX.writeFile(workbook, `listado_gastos_${moment(this.fechaDesde).format('YYYYMMDD')}_${moment(this.fechaHasta).format('YYYYMMDD')}.xlsx`);
+  }
+  
 
-  // initDataTable(data: BillViewOwner[]): void {
-  //   const table = $('#myTable');
-
-  //   if (($.fn as any).DataTable.isDataTable('#myTable')) {
-  //     $('#myTable').DataTable().clear().destroy();
-  //   }
-
-  //   this.dataTable = table.DataTable({
-  //     paging: true,
-  //     searching: true,
-  //     ordering: true,
-  //     lengthChange: false,
-  //     info: true,
-  //     pageLength: 10,
-  //     data: data,
-  //     columns: [
-  //       { data: 'description', title: 'Descripción' },
-  //       { data: 'category.description', title: 'Categoría' },
-  //       { data: 'providerId', title: 'Proveedor' },
-  //       { data: 'expenseType', title: 'Tipo de Gasto' },
-  //       {
-  //         data: 'expenseDate',
-  //         title: 'Fecha',
-  //         render: function(data) {
-  //           if (Array.isArray(data) && data.length === 3) {
-  //             const date = new Date(data[0], data[1] - 1, data[2]);
-  //             const day = date.getDate().toString().padStart(2, '0');
-  //             const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  //             const year = date.getFullYear();
-  //             return `${day}/${month}/${year}`;
-  //           }
-  //           return data;  //Funcion utilizada para formatear fecha a dd/mm/yyyy
-  //         }
-  //       },
-  //       {
-  //         data: 'amount',
-  //         title: 'Monto',
-  //         render: (data) => `$${data}`,
-  //       },
-  //     ],
-  //     language: {
-  //       processing: 'Procesando...',
-  //       search: 'Buscar:',
-  //       lengthMenu: 'Mostrar _MENU_ registros',
-  //       info: 'Mostrando del _START_ al _END_ de _TOTAL_ registros',
-  //       infoEmpty: 'Mostrando 0 registros',
-  //       infoFiltered: '(filtrado de _MAX_ registros totales)',
-  //       loadingRecords: 'Cargando...',
-  //       zeroRecords: 'No se encontraron resultados',
-  //       emptyTable: 'No hay datos disponibles en la tabla',
-  //       paginate: {
-  //         first: 'Primero',
-  //         previous: 'Anterior',
-  //         next: 'Siguiente',
-  //         last: 'Último',
-  //       },
-  //       aria: {
-  //         sortAscending: ': activar para ordenar la columna de manera ascendente',
-  //         sortDescending: ': activar para ordenar la columna de manera descendente',
-  //       },
-  //     },
-  //   });
-  // }
-  configDataTable(){
+  // Actualizar la tabla DataTable con los nuevos datos
+  configDataTable() {
+    console.log(this.bills);
     if ($.fn.DataTable.isDataTable('#myTable')) {
-      let table = $('#myTable').DataTable();
+      const table = $('#myTable').DataTable();
       table.clear();
       table.rows.add(this.bills);
       table.draw();
-      return;  // Salir de la función después de actualizar los datos
+      return;
     }
-    // Inicializar DataTables con los datos cargados
+  
     $('#myTable').DataTable({
       paging: true,
       searching: true,
       ordering: true,
       lengthChange: false,
-      info: true,
       pageLength: 10,
       data: this.bills,
       columns: [
@@ -259,36 +234,24 @@ export class ViewOwnerExpenseComponent implements OnInit {
           title: "Fecha", 
           data: "expenseDate", 
           render: function(data) {
-            // Convertir de 'YYYY-MM-DD' a 'DD/MM/YYYY' para la visualización
             return moment(data, 'YYYY-MM-DD').format('DD/MM/YYYY');
           },
-          type: 'date-moment' // Usamos el tipo 'date-moment' para la ordenación correcta
+          type: 'date-moment'
         },
         
       ],
       language: {
-        processing: 'Procesando...',
         search: 'Buscar:',
-        lengthMenu: 'Mostrar _MENU_ registros',
-        info: 'Mostrando del _START_ al _END_ de _TOTAL_ registros',
-        infoEmpty: 'Mostrando 0 registros',
-        infoFiltered: '(filtrado de _MAX_ registros totales)',
-        loadingRecords: 'Cargando...',
-        zeroRecords: 'No se encontraron resultados',
-        emptyTable: 'No hay datos disponibles en la tabla',
+        info: 'Mostrando _START_ a _END_ de _TOTAL_ registros',
         paginate: {
           first: 'Primero',
-          previous: 'Anterior',
-          next: 'Siguiente',
           last: 'Último',
+          next: 'Siguiente',
+          previous: 'Anterior'
         },
-        aria: {
-          sortAscending: ': activar para ordenar la columna de manera ascendente',
-          sortDescending: ': activar para ordenar la columna de manera descendente',
-        },
+        zeroRecords: 'No se encontraron resultados',
+        emptyTable: 'No hay datos disponibles'
       }
     });
-
-    
-  }
+  } 
 }
