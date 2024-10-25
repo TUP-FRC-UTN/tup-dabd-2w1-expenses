4796cd391597347f434f4e49b59dd64cf38dce16
113992-Menzi-Facundo <129486035+113992-Menzi-Facundo@users.noreply.github.com>
2024-10-24 22:04:22 -0300

    Navar Fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { ExpensesRegisterExpenseComponent } from './components/expenses-register-expense/expenses-register-expense.component';
 import { ViewGastosAdminComponent } from './components/expenses-view-gastos-admin/view-gastos-admin.component';
 import { ViewOwnerExpenseComponent } from './components/expenses-view-owner/view-owner-expense/view-owner-expense.component';
+import { ExpensesViewCategoryComponent } from './components/expenses-view-category/expenses-view-category.component';
 
 export const routes: Routes = [
   { path: 'viewExpenseAdmin', component:ViewGastosAdminComponent },
   { path: 'registerExpense', component: ExpensesRegisterExpenseComponent },
-  { path: 'viewExpenseOwner', component: ViewOwnerExpenseComponent }  
+  { path: 'viewCategory', component: ExpensesViewCategoryComponent },
+  { path: 'viewExpenseOwner', component: ViewOwnerExpenseComponent }
+  
 ];
