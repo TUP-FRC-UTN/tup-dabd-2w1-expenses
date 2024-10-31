c397ecdb375283e49620d69155b41db635126313
Mateo Di Francisco <111979@tecnicatura.frc.utn.edu.ar>
2024-10-31 03:37:50 -0300

    Reorganización de Components, Models y Services
    reorganización de componentes, modelos y servicios para que este mas ordenado el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ExpensesRegisterExpenseComponent } from './components/expenses-register-expense/expenses-register-expense.component';
-import { ViewGastosAdminComponent } from './components/expenses-view-gastos-admin/view-gastos-admin.component';
-import { ViewOwnerExpenseComponent } from './components/expenses-view-owner/view-owner-expense/view-owner-expense.component';
-import { ExpensesViewCategoryComponent } from './components/expenses-view-category/expenses-view-category.component';
-import { ReportExpenseComponent } from './components/expenses-report/report-expense/report-expense.component';
+import { ExpensesRegisterExpenseComponent } from './components/expenses-components/expenses-register-expense/expenses-register-expense.component';
+import { ViewGastosAdminComponent } from './components/expenses-components/expenses-view-expense-admin/expenses-view-expense-admin.component';
+import { ViewOwnerExpenseComponent } from './components/expenses-components/expenses-view-expense-owner/expenses-view-expense-owner.component';
+import { ExpensesViewCategoryComponent } from './components/expenses-components/expenses-view-category/expenses-view-category.component';
+import { ReportExpenseComponent } from './components/expenses-components/expenses-report/expenses-report.component';
 
 export const routes: Routes = [
   { path: 'viewExpenseAdmin', component: ViewGastosAdminComponent },
