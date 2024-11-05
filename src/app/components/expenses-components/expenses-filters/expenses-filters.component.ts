187a70e1e3f3d108a3664d510fd0c3a93b4a93e1
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-11-05 01:35:06 -0300

    remove unnecessary methods

--- src/app/components/expenses-components/expenses-filters/expenses-filters.component.ts
+++ src/app/components/expenses-components/expenses-filters/expenses-filters.component.ts
@@ -23,53 +23,22 @@ export class ExpensesFiltersComponent {
   @Output() selectedProvidersChange = new EventEmitter<Provider[]>();
   @Output() selectedTypesChange = new EventEmitter<ExpenseType[]>();
 
-  // Agregamos un debounceTime para evitar múltiples emisiones rápidas
-  private categoryChangeSubject = new Subject<void>();
-  private providerChangeSubject = new Subject<void>();
-
-  constructor() {
-    // Suscripción para cambios en categorías
-    this.categoryChangeSubject.pipe(
-      debounceTime(300)
-    ).subscribe(() => {
-      this.emitCategoryChange();
-    });
-
-    // Suscripción para cambios en proveedores
-    this.providerChangeSubject.pipe(
-      debounceTime(300)
-    ).subscribe(() => {
-      this.emitProviderChange();
-    });
-  }
 
   limpiarFiltros() {
     this.selectedCategories=[]
     this.selectedProviders=[]
     }
   onCategoryChange(): void {
-    this.categoryChangeSubject.next();
+    //this.categoryChangeSubject.next();
+    this.selectedCategoriesChange.emit(this.selectedCategories);
   }
 
   onProviderChange(): void {
-    this.providerChangeSubject.next();
+    //this.providerChangeSubject.next();
+    this.selectedProvidersChange.emit(this.selectedProviders)
   }
 
   onTypeChange():void{
     this.selectedTypesChange.emit(this.selectedTypes)
-  }
-
-  private emitCategoryChange(): void {
-    console.log('Emitting categories:', this.selectedCategories);
-    this.selectedCategoriesChange.emit([...this.selectedCategories]);
-  }
-
-  private emitProviderChange(): void {
-    const normalizedProviders = this.selectedProviders.map(provider => ({
-      ...provider,
-      id: Number(provider.id)
-    }));
-    console.log('Emitting providers:', normalizedProviders);
-    this.selectedProvidersChange.emit(normalizedProviders);
   }
 }
