b72675622e7bfd371cada3b97c101d5985360ab5
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-11-06 10:45:59 -0300

    standardized currency format

--- src/app/components/expenses-components/expenses-kpi/expenses-kpi.component.ts
+++ src/app/components/expenses-components/expenses-kpi/expenses-kpi.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { kpiExpense } from '../../../models/expenses-models/kpiExpense';
-import { CommonModule } from '@angular/common';
-
+import { CommonModule,registerLocaleData  } from '@angular/common';
+import localeEsAr from '@angular/common/locales/es-AR';
+registerLocaleData(localeEsAr, 'es-AR');
 @Component({
   selector: 'app-expenses-kpi',
   standalone: true,
   imports: [CommonModule],
+  providers:[{ provide: 'LOCALE_ID', useValue: 'es-AR' }],
   templateUrl: './expenses-kpi.component.html',
   styleUrl: './expenses-kpi.component.scss'
 })
